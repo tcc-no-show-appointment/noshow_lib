988b182368ab80ad381ddb04412b63650df18ee5
Paulo Tristao <pauloht3@gmail.com>
2025-11-20 14:58:35 -0300

    refactor: include build_features in the module's export

--- src/noshow_lib/__init__.py
+++ src/noshow_lib/__init__.py
@@ -1,4 +1,5 @@
 from .config import load_config
 from .data_processing import load_and_process_data
+from .feature_engineering import build_features
 
-__all__ = ["load_config", "load_and_process_data"]
+__all__ = ["load_config", "load_and_process_data", "build_features"]
